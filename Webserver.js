a63ae4a6fc1dfd9c7cb3e728972c73aea289053d
Hayden <112908824+HaydenD100@users.noreply.github.com>
2023-10-08 22:09:55 -0400

    Fully working project
    The names system has been fixed other then some ui style change the project is fully working as intended

--- Webserver.js
+++ Webserver.js
@@ -9,12 +9,17 @@ const fs = require('fs').promises;
 const requestListener = function (req, res) {
     console.log(req.url)
 
-    name = "anonymous"
+    
     
     if(req.url.substr(0,5) == "/send")
     {
 
         const fs = require('fs');
+        var namePos = req.url.search("name:")
+        var name = req.url.substr(namePos + 5);
+        if(name == "") name == "anonymous"
+        
+        
 
         
         fs.readFile('chats.txt', 'utf8', (err, data) => {
@@ -22,7 +27,7 @@ const requestListener = function (req, res) {
             console.error(err);
             return;
         }
-        fs.appendFile("chats.txt", "\n" + name + ":"+ decodeURI(req.url.substr(5)), (err) => { 
+        fs.appendFile("chats.txt", "\n" + name + ":"+ decodeURI(req.url.substr(5,namePos- 5)), (err) => { 
             if (err) 
               console.log(err); 
             
@@ -35,16 +40,12 @@ const requestListener = function (req, res) {
 
         res.writeHead(200);
 
-        res.end("<html><body><pre>" + data +"\n" + name + ":"+  decodeURI(req.url.substr(5)) +"<pre></body></html>");
+        res.end("<html><body><pre>" + data +"\n" + name + ":"+  decodeURI(req.url.substr(5,namePos - 5)) +"<pre></body></html>");
         
     });
 
     }
-    else if(req.url.substr(0,5) == "/name"){
-        if(req.url.substr(5) == "") name = "anonymous"
-        else name = req.url.substr(5);
-
-    }
+    
     else if(req.url.substr(0,5) == "/load")
     {
 
